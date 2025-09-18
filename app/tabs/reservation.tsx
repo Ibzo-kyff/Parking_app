4e0b9b7fcf3f57403b298bf695fedbbd01958a67
Moussa Kouyate <mobytech91@gmail.com>
2025-09-18 17:33:55 +0200

    retouche des reservations

--- app/tabs/reservation.tsx
+++ app/tabs/reservation.tsx
@@ -1,17 +1,31 @@
 import React from "react";
-import ReservationList from "../../components/screens/Reservation";
 import {
-  getReservationsParking,
-  cancelReservationApi,
-} from "../../components/services/reservationApi";
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  TextInput,
+  ScrollView,
+  ActivityIndicator,
+} from 'react-native';
+import ReservationList from "../../components/screens/Reservation";
+import { getUserReservations, cancelReservationApi } from "../../components/services/reservationApi";
+import Header from "../Header";
 
-const ReservationsParking = () => {
+const ReservationsUser = () => {
   return (
+        <View style={styles.container}>
+      <Header />
     <ReservationList
-      fetchReservations={getReservationsParking}
+      fetchReservations={getUserReservations}
       cancelReservation={cancelReservationApi}
     />
+        </View>
   );
 };
+const styles = StyleSheet.create({
+  container: {  backgroundColor: '#f4f3f3', padding: 20, flex: 1},
 
-export default ReservationsParking;
+})
+export default ReservationsUser;
